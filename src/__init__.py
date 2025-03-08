10378dc838458de248577be77fb846d9d5bc7f5d
Pau Carrillo Velasco <pau.carrillovelasco@student.manchester.ac.uk>
2025-03-08 21:23:22 +0000

    setting up git and venv

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+print("Initialising Polidex...  --  This project was made by Pau :)")
+
+# this file will be run when the package is imported by others, for example
+
+# i dont think it'll do anything for now since only I am using it
+
+
+# for venv:
+
+# Set-ExecutionPolicy Unrestricted -Scope Process
+# .venv\Scripts\activate
---
+++ src/utils.py
@@ -0,0 +1,3 @@
+# could include any helper functions used by any other modules
+
+# if there are too many or different types, I could modularise into a utils folder and have individual helpers inside
