262713acbc17554ad7100cecb2b65df146316020
Pau PC <paucv27@gmail.com>
2025-06-29 17:41:16 +0100

    mostly working, name recognition gets weird with lots of noise

--- src/recognition.py
+++ src/recognition.py
@@ -1,9 +1,9 @@
-import easyocr
+import re
 import cv2
+import easyocr
 import pandas as pd
 import matplotlib.pyplot as plt
 from glob import glob
-from pprint import pprint
 
 # IMPORT ALL IMAGES FROM DATASET ----------------------------
 
@@ -15,76 +15,97 @@ test_path = images[161]
 test_filename = images[161].split('\\')[-1]
 
 img = cv2.imread(test_path)
+img = cv2.resize(img, (img.shape[1]*3, img.shape[0]*3), interpolation=cv2.INTER_CUBIC)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 print('Filepath: ',test_path)
 print('Filename: ',test_filename)
 
-# PRE-PROCESS SECTIONS --------------------------------------
+# SECTIONS ---------------------------------------------------
 
-name_section = img[0:140, 0:500].copy()
-name_section = cv2.resize(name_section, (name_section.shape[1]*2, name_section.shape[0]*2), interpolation=cv2.INTER_CUBIC)
+name_section = img[0:140*3, 0:500*3].copy()
 name_section = cv2.cvtColor(name_section, cv2.COLOR_RGB2GRAY)
-# _, name_section = cv2.threshold(name_section, 180, 255, cv2.THRESH_BINARY)
+_, name_section = cv2.threshold(name_section, 40, 255, cv2.THRESH_BINARY_INV)
 
-number_section = img[970:990, 120:200].copy()
-number_section = cv2.resize(number_section, (number_section.shape[1]*5, number_section.shape[0]*5), interpolation=cv2.INTER_CUBIC)
-# number_section = cv2.cvtColor(number_section, cv2.COLOR_RGB2GRAY)
-# _, number_section = cv2.threshold(number_section, 240, 255, cv2.THRESH_BINARY_INV)
-number_section = cv2.GaussianBlur(number_section, (3,3), 0)
-# number_section = cv2.erode(number_section, (5,5), iterations=3)
+number_section = img[960*3:1000*3, 115*3:205*3].copy()
+number_section = cv2.cvtColor(number_section, cv2.COLOR_RGB2GRAY)
+_, number_section = cv2.threshold(number_section, 30, 255, cv2.THRESH_BINARY_INV)
 
 # GET RESULTS FROM EASYOCR -----------------------------------
 
 reader = easyocr.Reader(['en'], gpu=False)
 
-full_results = reader.readtext(img)
+#full_results = reader.readtext(img)
 name_results = reader.readtext(name_section)
 number_results = reader.readtext(number_section)
 
 # SHOW DATAFRAME FOR BBOXs -----------------------------------
 
-full_df = pd.DataFrame(full_results, columns=['BBOX','TEXT','CONF'])
+#full_df = pd.DataFrame(full_results, columns=['BBOX','TEXT','CONF'])
 name_df = pd.DataFrame(name_results, columns=['BBOX','TEXT','CONF'])
 number_df = pd.DataFrame(number_results, columns=['BBOX','TEXT','CONF'])
 
-print(full_df)
+#print(full_df)
 print(name_df)
 print(number_df)
 
 # DRAW BBOXs -------------------------------------------------
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-for result in full_results:
+# font = cv2.FONT_HERSHEY_SIMPLEX
+# for result in full_results:
 
-    top_left = tuple(int(coord) for coord in result[0][0])
-    bottom_right = tuple(int(coord) for coord in result[0][2])
-    text = result[1]
+#     top_left = tuple(int(coord) for coord in result[0][0])
+#     bottom_right = tuple(int(coord) for coord in result[0][2])
+#     text = result[1]
 
-    img = cv2.rectangle(img, top_left, bottom_right, (0,255,0), 5)
-    img = cv2.putText(img, text, top_left, font, .5, (255,255,255), 2, cv2.LINE_AA)
+#     img = cv2.rectangle(img, top_left, bottom_right, (0,255,0), 5)
+#     img = cv2.putText(img, text, top_left, font, .5, (255,255,255), 2, cv2.LINE_AA)
 
 fig, axs = plt.subplots(1,3, figsize=(15,5))
 
 axs[0].imshow(img)
 axs[0].set_title('Full')
 
-axs[1].imshow(name_section)
+axs[1].imshow(name_section, cmap='gray')
 axs[1].set_title('Name')
 
-axs[2].imshow(number_section)
+axs[2].imshow(number_section, cmap='gray')
 axs[2].set_title('Number')
 
 plt.show()
 
-return_text = ''
+def process_name():
 
-for result in name_results:
+    text = ''
+    results_popped = 0
 
-    return_text += result[1]+' '
+    for result in name_results:
 
-for result in number_results:
+        text = text+result[1]+' '
+        results_popped += 1
 
-    return_text += result[1]+' '
+        if results_popped == 1:
+            break
+    
+    text = re.sub(r'[^a-zA-Z ]', '', text)
 
-print(return_text)
+    return text
+
+def process_number():
+
+    text = ''
+
+    for result in number_results:
+
+        filtered = re.sub(r'[^0-9/]', '', result[1])
+        text += filtered+' '
+
+    if '/' not in text:
+        text = text[:3] + '/' + text[3:]
+
+    return text
+
+clean_name = process_name()
+clean_number = process_number()
+
+print(clean_name,clean_number)
